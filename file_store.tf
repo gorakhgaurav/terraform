eac68b91bdc5ca32bf9d314e91c125b302e05ff9
gorakhgaurav <139329569+gorakhgaurav@users.noreply.github.com>
2025-02-25 21:18:53 +0530

    Create file_store.tf

---
+++ file_store.tf
@@ -0,0 +1,13 @@
+resource "google_filestore_instance" "filestore_instance" {
+  name       = var.filestore_name
+  project    = var.project_id
+  zone       = var.zone
+  tier       = var.tier
+  networks {
+    network = var.network
+  }
+  file_shares {
+    name       = var.share_name
+    capacity_gb = var.capacity_gb
+  }
+}
