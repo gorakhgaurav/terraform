a3fdf166ddb3c45a8ca821d77410444a19acf51a
gorakhgaurav <139329569+gorakhgaurav@users.noreply.github.com>
2025-02-25 09:57:58 +0530

    Update varaible.tf

--- varaible.tf
+++ varaible.tf
@@ -18,3 +18,26 @@ variable "service_account" {
   description = "Service account email for Pub/Sub access"
   type        = string
 }
+variable "disk_name" {
+  description = "Name of the persistent disk"
+  type        = string
+  default     = "gke-block-storage"
+}
+
+variable "disk_type" {
+  description = "Type of persistent disk (pd-standard, pd-ssd, pd-balanced)"
+  type        = string
+  default     = "pd-balanced"
+}
+
+variable "disk_size" {
+  description = "Size of the persistent disk in GB"
+  type        = number
+  default     = 50
+}
+
+variable "zone" {
+  description = "GCP Zone for the disk"
+  type        = string
+  default     = "us-central1-a"
+}
