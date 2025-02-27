aec1e05b9f590e9c57f257de13a24045734e6f30
gorakhgaurav <139329569+gorakhgaurav@users.noreply.github.com>
2025-02-27 17:04:52 +0530

    Update file_store.tf

--- file_store.tf
+++ file_store.tf
@@ -12,13 +12,5 @@ resource "google_filestore_instance" "filestore_instance" {
   file_shares {
     name         = var.share_name
     capacity_gb  = var.capacity_gb
-    kms_key_name = var.keys  # <-- Enable KMS encryption
-
-    export_options {
-      access_mode   = "READ_WRITE"
-      squash_mode   = "NO_ROOT_SQUASH"
-      anon_uid      = 65534
-      anon_gid      = 65534
-    }
   }
 }
