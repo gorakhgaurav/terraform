f53a570b0528dbe4e65f1a58371a1a75cceffaaf
gorakhgaurav <139329569+gorakhgaurav@users.noreply.github.com>
2025-03-10 18:20:24 +0530

    Update varaible.tf

--- varaible.tf
+++ varaible.tf
@@ -87,26 +87,6 @@ variable "keys" {
 ######### RBAC
 
 
-
-variable "project_id" {
-  description = "GCP project ID"
-  type        = string
-}
-
-variable "region" {
-  description = "Region where GKE cluster is deployed"
-  type        = string
-}
-
-variable "gke_service_account" {
-  description = "Service account email for GKE nodes"
-  type        = string
-}
-
-variable "gke_admin_user" {
-  description = "User who will be given Kubernetes admin access"
-  type        = string
-}
 variable "namespaces" {
   description = "List of namespaces for user-group read-only access"
   type        = list(string)
