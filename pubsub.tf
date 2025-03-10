72f1edbaf3c7fb0ecc4771c70c1d6d5a0f10d356
gorakhgaurav <139329569+gorakhgaurav@users.noreply.github.com>
2025-03-10 11:37:47 +0530

    Update pubsub.tf

--- pubsub.tf
+++ pubsub.tf
@@ -1,3 +1,8 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
 data "google_pubsub_topic" "existing_topic" {
   name    = module.example_topic.topic 
   labels = local.labels
